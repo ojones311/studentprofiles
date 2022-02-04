318eb6f9c48a47583325e2991b56c5b94fc7c6bd
Owen Jones <owenthefirst93@gmail.com>
2022-02-04 13:25:57 -0500

    implemented filter search

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -8,13 +8,14 @@ import Searchbar from './Searchbar'
 const Dashboard = () => {
 
     const [studentList, setStudentList] = useState(null)
+    const [filteredList, setFilteredList] = useState(null)
     const [input, setInput] = useState('')
 
     const fetchStudentList = async () => {
         try{
            const response = await fetch(`https://api.hatchways.io/assessment/students`)
            const students = await response.json()
-           console.log(students)
+        //    console.log(students)
            setStudentList(students)
         }catch(error){
             console.log('error: ', error)
@@ -23,10 +24,13 @@ const Dashboard = () => {
 
     //Loop through student obj and see if searchTerm is included in the name 
     const updateSearch = (input) => {
-        let filteredList =  studentList.filter((elem) => {
-            
+        // console.log(studentList.students)
+        let filteredList = studentList.students.filter((elem) => {
+            let fullName = elem.firstName + elem.lastName
+            return fullName.toLowerCase().includes(input.toLowerCase())
         })
         setInput(input)
+        setFilteredList(filteredList)
     }
 
     useEffect(async() => {fetchStudentList()}, [])
@@ -37,8 +41,18 @@ const Dashboard = () => {
                 input={input} 
                 onChange={updateSearch}
             />
-            <div className='main'>
+            {/* <div className='main'>
                 {studentList && studentList.students.map(elem => (
+                    <>
+                        <StudentCard 
+                            key={elem.id} 
+                            student={elem}
+                        />
+                    </>
+                ))}
+            </div> */}
+            <div className='main'>
+                {filteredList && filteredList.map(elem => (
                     <>
                         <StudentCard 
                             key={elem.id} 
